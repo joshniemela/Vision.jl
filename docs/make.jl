826ef1f528b8c24a9d02eff8f8481edc41770ab5
Joshua Niemelä <josh@jniemela.dk>
2022-11-27 17:56:36 +0100

    Added documenter.jl

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter
+using Vision
+
+makedocs(
+    sitename = "Vision",
+    format = Documenter.HTML(),
+    modules = [Vision]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
---
+++ docs/src/index.md
@@ -0,0 +1,29 @@
+# Vision.jl
+
+```@contents
+```
+
+Documentation for Vision.jl
+
+## Functions
+
+```@docs
+makeRequestBody
+```
+
+```@docs
+visionFeature
+```
+
+```@docs
+getResponse
+```
+
+```@docs
+parseFeatures(responseBody)
+```
+
+## Index
+
+```@index
+```
